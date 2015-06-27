146685a1fbb026a232c3448d9ac2ca1ca58e50c6
BChangs <lanchang@princeton.edu>
2015-06-27 14:47:23 -0400

    Update GameMode.cpp

--- GameMode.cpp
+++ GameMode.cpp
@@ -11,5 +11,9 @@ GameMode::~GameMode(void) {
 }
 
 void GameMode::GameLoop(void) {
-    
+    while (gameMode && check->type != SDL_QUIT) {
+        SDL_PollEvent(Setup.checkGet());
+        SDL_RenderClear(Setup.renderGet());
+        SDL_RenderPresent(Setup.renderGet());
+    }
 }
