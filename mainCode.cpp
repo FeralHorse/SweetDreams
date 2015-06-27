01257da53f44a92db2ada91c8eca3672f61792cd
BChangs <lanchang@princeton.edu>
2015-06-27 15:17:15 -0400

    Update mainCode.cpp

--- mainCode.cpp
+++ mainCode.cpp
@@ -5,39 +5,10 @@
 
 using namespace std;
 
-const int ScreenWidth = 800; //subject to change
-const int ScreenHeight = 600; //subject to change
-
 int main() {
     
-    //Intiating game window
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = NULL;
-    window = SDL_CreateWindow( "SDL Tutorial", 100, 100, ScreenWidth, ScreenHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    bool gameMode = true;
-    
-    //Error check
-    if (window == NULL) {
-        cout << "Error: Could not create window";
-        return 0;
-    }
-    
-    //Renderer and event to update window
-    SDL_Renderer* renderer = NULL;
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Event* check = new SDL_Event();
-    
-    //Game Mode
-    while (gameMode && check->type != SDL_QUIT) {
-        SDL_PollEvent(check);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-    }
-    
-    //Clearing memory
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
-    delete check;
+    GameMode gameMode;
+    gameMode.GameLoop();
     
     //Random Outro
     cout << "Feral Horse!\n";
