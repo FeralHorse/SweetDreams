4fb3567f089f0924692bc353fdd2a4b794ec5051
BChangs <lanchang@princeton.edu>
2015-06-27 15:17:59 -0400

    Update Setup.cpp

--- Setup.cpp
+++ Setup.cpp
@@ -1,16 +1,13 @@
 #include "Setup.h"
+#include <SDL2/SDL.h> //May change depending on comp
+#include <SDL2_image/SDL_image.h>//May change depending on comp
+
 
 //Constructor
 Setup::Setup(void) {
-    window = SDL_CreateWindow( "SDL Tutorial", 100, 100, ScreenWidth, ScreenHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    window = SDL_CreateWindow( "SDL Tutorial", 100, 100, 600, 400, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     check = new SDL_Event();
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    
-    //Error check
-    if (window == NULL) {
-        cout << "Error: Could not create window";
-        return 0;
-    }
 }
 
 //Deconstructor
