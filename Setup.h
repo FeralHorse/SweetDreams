48dca89d0d78b39e11ed852d7f08ff158d08b3aa
BChangs <lanchang@princeton.edu>
2015-06-27 15:17:34 -0400

    Update Setup.h

--- Setup.h
+++ Setup.h
@@ -1,16 +1,18 @@
 #pragma once //Goes through this code once
+#include <SDL2/SDL.h> //May change depending on comp
+#include <SDL2_image/SDL_image.h>//May change depending on comp
 
 class Setup {
-   
+    
 public:
     Setup(void);
     ~Setup(void);
     SDL_Renderer* renderGet(void);
     SDL_Event* eventGet(void);
     
-//Declarations, private for security
+    //Declarations, private for security
 private:
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Event* check;
-}
+};
