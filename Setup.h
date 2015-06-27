27a9bc63a52a58852174bc025fabe2d1ec6aab04
BChangs <lanchang@princeton.edu>
2015-06-27 14:30:39 -0400

    Update Setup.h

--- Setup.h
+++ Setup.h
@@ -1,1 +1,13 @@
+#pragma once //Goes through this code once
 
+class Setup {
+   
+public:
+    Functions(void);
+    ~Functions(void);
+    
+//Declarations, private for security
+private:
+    
+    
+}
