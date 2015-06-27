2a8402c959cb5c807873e05f405c8a02744cceff
BChangs <lanchang@princeton.edu>
2015-06-27 14:44:19 -0400

    Update Setup.cpp

--- Setup.cpp
+++ Setup.cpp
@@ -2,7 +2,15 @@
 
 //Constructor
 Setup::Setup(void) {
+    gameMode = true;
+    window = SDL_CreateWindow( "SDL Tutorial", 100, 100, ScreenWidth, ScreenHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     
+    //Error check
+    if (window == NULL) {
+        cout << "Error: Could not create window";
+        return 0;
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
 //Deconstructor
