907e0983aa02815ef43d8199d2d7c34b5117d017
BChangs <lanchang@princeton.edu>
2015-06-29 23:23:28 -0400

    added begin function to simplify

--- Setup.h
+++ Setup.h
@@ -6,6 +6,7 @@ class Setup {
 public:
     Setup(void); //Constructor
     ~Setup(void); //Deconstructor
+    void begin(void); //Function that begins the game window
     SDL_Renderer* renderGet(void); //Function to get renderer
     SDL_Event* eventGet(void); //Function to get event
     
