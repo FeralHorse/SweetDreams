54d42e1dbd591185c91a3b142a05e4c7d40dcba4
BChangs <lanchang@princeton.edu>
2015-07-09 20:16:17 -0400

    defined functions and variables

--- images.h
+++ images.h
@@ -1,1 +1,16 @@
+#pragma once //Goes through this code once
+#include "stdafx.h"
 
+class images {
+    
+public:
+    images(SDL_Renderer* renderer, std:: string file, int x, int y, int w, int h); //Constructor
+    ~images(void); //Deconstructor
+    void draw(void);
+    
+    //Declarations, private for security
+private:
+    SDL_Texture* player;
+    SDL_Rect playerRect;
+    SDL_Renderer* renderer;
+};
