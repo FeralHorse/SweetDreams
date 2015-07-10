5de33a77a2bf74e4eaf1439924e660eacc3aded6
BChangs <lanchang@princeton.edu>
2015-07-09 20:19:27 -0400

    wrote in functions

--- images.cpp
+++ images.cpp
@@ -1,1 +1,26 @@
+#include "images.h"
+#include "stdafx.h"
+
+
+//Constructor
+images::images(SDL_Renderer* inputRend, std:: string file, int x, int y, int w, int h) {
+    renderer = inputRend;
+    subject = NULL;
+    subject = IMG_LoadTexture(renderer, file.c_str());
+    subjectRect.x = x;
+    subjectRect.y = y;
+    subjectRect.w = w;
+    subjectRect.h = h;
+}
+
+//Deconstructor
+images::~images(void) {
+    SDL_DestroyTexture(subject);
+    SDL_DestroyRenderer(renderer);
+}
+
+//Function to display character
+void images::draw(void) {
+    SDL_RenderCopy(renderer, subject, NULL, &subjectRect);
+}
 
